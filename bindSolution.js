f22aad1cb13a3270240332d410cc4f2dee098968
sebastiangilpatino <sebastiangilpatino@gmail.com>
2021-04-27 18:25:38 -0500

    same solution diff ways

---
+++ bindSolution.js
@@ -0,0 +1,22 @@
+function askPassword(ok, fail){
+    let password = prompt("Password?",'');
+    if (password=='rockstar'){
+        ok();
+    }else{
+        fail();
+    }
+}
+
+let user = {
+    name: 'Jhon',
+    
+    loginOk(){
+        alert(`${this.name} looged in`);
+    },
+
+    loginFail(){
+        alert(`${this.name} failed to log in`);
+    },
+};
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
